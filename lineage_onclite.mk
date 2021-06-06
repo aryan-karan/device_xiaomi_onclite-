d241df09c9d2fac374338ea4ca5afb3529617779
Aryan Karan <76065891+aryankaran@users.noreply.github.com>
2021-06-06 21:26:40 +0530

    Using redfin May Fingerprint

--- lineage_onclite.mk
+++ lineage_onclite.mk
@@ -23,9 +23,9 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "xiaomi/onc/onc:9/PKQ1.181021.001/V11.0.3.0.PFLMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="onc-user 9 PKQ1.181021.001 V11.0.3.0.PFLMIXM release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210405.005 7181113 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
