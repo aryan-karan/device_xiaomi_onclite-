1d2f83c66ea618a385fe2b9cdc64d6f54fa5461a
Aryan Karan <aryankaran28022004@gmail.com>
2021-10-10 23:08:48 +0530

    Declutter

--- havoc_onclite.mk
+++ havoc_onclite.mk
@@ -20,11 +20,6 @@ TARGET_BOOT_ANIMATION_RES := 720
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_GAPPS_ARCH := arm64
 
-# Official tags
-HAVOC_BUILD_TYPE := Official
-HAVOC_MAINTAINER := Aryan-Karan-(aryankaran)
-HAVOC_GROUP_URL := https://t.me/havoc_os_onclite
-
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := havoc_onclite
 PRODUCT_DEVICE := onclite
